b7f6401a6d04d3195c056374122653a2a85ade7e
Nagham malaeb <na8ammalaeb@gmail.com>
2025-09-13 16:30:39 +0300

    Updated and tested

--- test-mongo.js
+++ test-mongo.js
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: ".env.local" });
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB!"))
-  .catch((err) => console.error("Connection error:", err));
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in .env.local");
+}
+
+async function main() {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB connected successfully!");
+    process.exit(0);
+  } catch (error) {
+    console.error("Connection error:", error);
+    process.exit(1);
+  }
+}
+
+main();
