fa150d1e1a944b131d98fd2aec8399bf43326efc
MayaSemaan <mayasemaan33@gmail.com>
2025-09-22 22:14:11 +0300

    clean version

--- app/api/register/route.js
+++
@@ -1,9 +0,0 @@
-import dbConnect from "../../../utils/dbConnect";
-import User from "../../../smart-medicine/models/user";
-
-export async function POST(req) {
-  await dbConnect();
-  const data = await req.json();
-  const newUser = await User.create(data);
-  return new Response(JSON.stringify(newUser), { status: 201 });
-}
